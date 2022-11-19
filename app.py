d07394612454d38da62ef24a7cf104b94311c7f1
Hassan Khalid Butt <55914809+hbutt877@users.noreply.github.com>
2022-11-19 01:33:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, Python World!"
+def index():
+    return 'Enter prompt in url'
+
+@app.route("/<prompt>")
+def dall_e(prompt):
+    openai.api_key = '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'
+    response = openai.Image.create(
+      model='image-alpha-001',
+      prompt=prompt,
+      size='1024x1024',
+      n=1
+    )
+    return response['data'][0]['url']
