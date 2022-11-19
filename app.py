05adb3e1ee4c90f5f282847167a8be24076a44df
Hassan Khalid Butt <55914809+hbutt877@users.noreply.github.com>
2022-11-19 02:21:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 
 @app.route("/<prompt>")
 def dall_e(prompt):
-    openai.api_key = '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'
+    openai.api_key = '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'
     response = openai.Image.create(
       model='image-alpha-001',
       prompt=prompt,
